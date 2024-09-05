a1d88545e5840c8b7a807d3a81008ead70328435
sMC-Lean <125336469+sMC-Lean@users.noreply.github.com>
2024-09-06 00:20:51 +1000

    parents and students text added to object, coaches notes will require more work to include the images etc via links in a template literal

--- data.js
+++ data.js
@@ -1,9 +1,79 @@
 "use strict";
 
 const modalText = {
-  parents: "parent text",
-  students: "student text",
-  coaches: "coaches text",
+  parents: `Welcome to Summit BJJ. We understand that things on the mats may look weird, and this page is designed to give you an idea of what we believe, and what we hope your child will achieve here.
+
+"Self-defence is not a set of techniques, it’s a state of mind and it begins with the belief that you are worth defending." Rorion Gracie
+
+When we bring our children to learn self-defence, it isn’t always obvious what that means in practice. 
+
+Self-defence is a highly personal journey that will involve different challenges for everyone. Something that may seem easy to one student may be out of reach for another. It is not wholly physical; in fact, the mental aspects of our training are where the most benefit can be found.
+
+It is our goal to remember that failure is CRITICAL to the learning process. Students will fail often and sometimes in spectacular fashion. 
+
+Learning must be broken down into small, understandable steps and it must be practiced consistently over time.
+
+If we want the kids to continuously come to training, and fail frequently while practicing repetitively, then TRAINING MUST ALWAYS BE FUN.
+
+Because the student’s mental skills are the focus here, we tailor the training to promote those.
+
+We aim to foster:
+	Improved self confidence
+	Improved self-respect
+    Improved respect for others
+	Healthy relationships and communication
+	Kindness
+	Impulse control and discipline
+	Learning skills
+	Ability to set boundaries and make decisions
+	Etc Etc.
+
+These skills take years to master, and can sometimes be very large changes. Behavioural change is difficult and will happen slowly over time, with consistent practice.
+
+The coaches here will try to do this with a “pull” teaching style. Positive reinforcement has been shown to be far more effective at engaging the types of change that we aim for. This means we will aim to have students attend many, many classes and make only a small change at any one time. This might be only a small piece of advice or a small behavioural intervention. For the physical practice we can let students experiment and play, and only correct a small part of a technique at any one time.
+We will encourage behaviours or skills that are positive, and we will gloss over or sometimes downright ignore a behaviour that we would like to see stop. This may mean that a student may not be focussing right now, or that their technique may not be correct. Our goal is to help support children to grow into competent and confident humans in the future, and so we will gradually address those things over time. Obviously, there are exceptions to this for safety, or for students who have been training for a long time. As a student’s confidence grows the techniques we use can evolve as well.
+
+For you as a parent, there are a some things that we ask you to remember:
+
+The Kids training room is not competition.
+	The Gracie family believed that Jiu Jitsu for children should be play. They created what can only be described as the best stable of Jiu Jitsu competitors in history. We agree with this. The students should feel like this is fun. Some days it will be more challenging, and at some points a student may feel claustrophobic, but it should never feel like they are fighting for their survival.
+Please do not coach the students from the sidelines, or cheer them on as if it is competitive.
+
+We are trying to steer the ship gradually.
+	Unless we ask you to intervene with your child, or there is an urgent need, please do not try to correct their behaviour from the sidelines. Students have many things to think about and many voices can be distracting. We know the classes may look chaotic! Over time a student’s behaviour will gradually align with the group. Small children are allowed some chaos.
+
+Consistency is Key.
+	Please try to remember our approach. We ask that you don’t correct behaviours between rounds or after class. We encourage that you try to notice something positive and offer praise on that aspect. If they were kind, or helped another student. If they persisted, if they were very focussed. If they were having fun with their training partner. 
+
+Technique will come naturally to them.
+	If classes are fun, the learning is within their capability and students train consistently, they will get better at Jiu Jitsu very quickly. Children have amazing brains that learn very effectively. We try to let students experiment as much as possible and have fun trying to unravel the patterns of Jiu Jitsu. They will do whacky stuff, and some of it won’t work at all. Please do not try to correct the things that could be better. Encourage the cool and creative things, praise positive outcomes, but mainly praise effort and persistence.
+
+If any of this doesn’t make sense, or if you or your child would like to ask any questions, please don’t hesitate to approach Brian or the coaching staff after class.
+`,
+  students: `Summit kid’s warrior code:
+
+Jiu Jitsu is a superpower, and I intend to use it for good. I am here to train my mind and body, and shape myself into the person I want to be.
+
+In order to reach my potential as a complete warrior, I will always try to follow this code.
+
+	I will show respect to the people I practice with, treat them with kindness and never put them down or criticise them. I will do my best to listen respectfully and focus.
+
+	I will do my best to keep my training partners safe, and to remember that we are here to help each other learn.
+
+	I will try to make sure that I have fun, and that my training partners do too.
+ 
+	I will always try my best and hardest, and do my best not to give up.
+
+	I know that I can only control myself, and I will do my best to keep doing the right thing even if someone else is not. I will never, ever use the things I learn to be a bully, only for safety and protection.
+
+	I will make mistakes, and I will do my best to take responsibility for learning from them. But I promise to treat myself with the same kindness and respect I show to the people around me when I do, and to try to forgive other people when they make mistakes.
+
+	I know that a warrior is disciplined, so I will use these same rules everywhere. At Jiu Jitsu, at school and at home.
+
+If we all do these things together we can train hard, wrestle and have fun, and I will learn how to be a strong and confident grappler, and a strong and confident human. 
+`,
+  coaches: `coaches 
+            text`,
 };
 
 export { modalText };
